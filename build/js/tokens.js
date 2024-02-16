a638f1e90c32488f207a8c808518617db697d35c
marcelorpc <marcelorpchaves@gmail.com>
2024-02-16 07:00:32 -0300

    initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,16 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 16 Feb 2024 09:54:38 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray500": "#6b7280",
+  "ColorGray900": "#111827",
+  "ColorGray950": "#030712",
+  "ColorBlue500": "#3b82f6",
+  "VariableCollectionGray500": "#6b7280ff",
+  "VariableCollectionGray900": "#111827ff",
+  "VariableCollectionWhite": "#ffffffff",
+  "VariableCollectionBlue500": "#3b82f6ff"
+};
